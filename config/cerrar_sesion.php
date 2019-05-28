f0f90c8052929c7262ca802cb9a004e9f1d39877
jcallec7 <jcallec7@hotmail.com>
2019-05-28 12:42:56 -0500

    rutas relativas

--- config/cerrar_sesion.php
+++ config/cerrar_sesion.php
@@ -1,5 +1,5 @@
 <?php
     session_start();
     session_destroy();
-    header("Location: /Practicas/Proyecto/public/vista/elegir_local.php");
+    header("Location: ../public/vista/elegir_local.php");
 ?>
