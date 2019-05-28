92d2e3a3d97d37bc3429a040bd09d522730c6317
jcallec7 <jcallec7@hotmail.com>
2019-05-28 11:59:20 -0500

    añadir rutas relativas para evitar conflictos

--- public/vista/productos/productos.php
+++ public/vista/productos/productos.php
@@ -8,7 +8,7 @@
     <body>
         <header>
             <div class="topHeader">
-                <a href="mi_cuenta.html" class="nombreUser">INICIAR SESION O REGISTRARME</a>
+                <a href="../mi_cuenta.html" class="nombreUser">INICIAR SESION O REGISTRARME</a>
             </div>
             <div class="encabezado">
                 <nav class="menu">
@@ -54,22 +54,23 @@
                     include '../../../config/conexionBD.php';  
                     $sql = "SELECT * FROM productos"; 
                     $result = $conn->query($sql); 
-                    //$sql = 'SELECT = FROM news WHERE status <> 0'; 
         
                     if ($result->num_rows > 0) { 
                             
                         while($row = $result->fetch_assoc()) { 
 
                             echo "<article class='contenidoProductos'>";
-                            ?>                  
-                            <img id= "imagenProd" src="<?php echo $row['prod_imagen']?>" alt=imgProd>
-                            <?php
-                            echo "<br>";        
-                            echo $row['prod_nombre']; 
-                            echo "<br>";     
-                            echo "$".$row['prod_precio']." INCLUYE IVA"; 
-                            echo "<br>";
-                            echo "<a href=''><img class = 'imgCarrito' src='../../../imagenes/iconos/carrito.png' alt='imgCarro'> </a>";
+
+                                ?>                  
+                                <img id= "imagenProd" src="<?php echo $row['prod_imagen']?>" alt=imgProd>
+                                <?php
+                                echo "<br>";        
+                                echo $row['prod_nombre']; 
+                                echo "<br>";     
+                                echo "$".$row['prod_precio']." INCLUYE IVA"; 
+                                echo "<br>";
+                                echo "<a href=''><img class = 'imgCarrito' src='../../../imagenes/iconos/carrito.png' alt='imgCarro'> </a>";
+
                             echo "</article>";                                             
                             
 
