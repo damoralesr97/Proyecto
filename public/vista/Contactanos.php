1ac85de1c964ff8c03f124e3579065ae0c7214da
esteban rosero <davidesteb98@gmail.com>
2019-05-29 13:11:44 -0500

    Creacion de contactanos

---
+++ public/vista/Contactanos.php
@@ -0,0 +1,95 @@
+<?php
+    session_start();
+    $_SESSION["local"];
+?>
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Quienes Somos</title>
+        <link type="text/css" href="../../css/estilos.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <div class="topHeader">
+                <a href="mi_cuenta.php" class="nombreUser">INICIAR SESION O REGISTRARME</a>
+            </div>
+            <div class="encabezado">
+                <nav class="menu">
+                    <ul>
+                        <li><a href="home.php?codigo=<?php echo $_SESSION["local"] ?>">INICIO</a></li>
+                        <li><a href="">NOSOTROS</a>
+                            <ul>
+                                <li><a href="">QUIENES SOMOS</a></li>
+                                <li><a href="">MISION Y VISION</a></li>
+                                <li><a href="">HISTORIA</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="productos.php">PRODUCTOS</a></li>
+                        <li><a href="">CONTACTO</a></li>
+                    </ul>
+                </nav>
+                <div class="busqueda">
+                    <input type="search" name="buscar" id="buscar" placeholder="Buscar producto">
+                    <a href="">Buscar</a>
+                </div>
+                <div class="carrito">
+                    <img src="../../imagenes/iconos/carrito.png" alt="imgCarro">
+                    <a href="">Carrito</a>
+                    <i id="precio">$ 0.00</i>
+                </div>
+            </div>
+        </header>
+            <div>
+                <article>
+                    <div>
+                            <article class="imagenesContenido">
+                            <img src="../../imagenes/img/imgCont1.png" alt="Imagenes promociones" class="imagenCabecera">
+                        </article>
+                    </div>
+                    <h1 class="tituloContenido">Contactanos:</h1>
+                    <p class="contenidoTexto">
+                    <div class="contenidoPie">
+                    <div class="infoPie">
+                        <h3>INFORMACION DE CONTACTO</h3>
+                        <div class="detalleInfoPie">
+                            <p class="tituloDetalleInfoPie">DIRECCION</p>
+                            <p class="textoDetalleInfoPie">Av. Huayna Capac y Pio Bravo. Matriz Cuenca</p>
+                        </div>
+                        <div class="detalleInfoPie">
+                            <p class="tituloDetalleInfoPie">TELEFONO</p>
+                            <p class="textoDetalleInfoPie">(07) 4123150</p>
+                        </div>
+                        <div class="detalleInfoPie">
+                            <p class="tituloDetalleInfoPie">EMAIL</p>
+                            <p class="textoDetalleInfoPie">servicio@ferreteria.com</p>
+                        </div>
+                    </div>
+                    <div class="infoPie">
+                        <h3>ENCUENTRANOS</h3>
+                        <div class="detalleInfoPie">
+                            <p class="tituloDetalleInfoPie">MATRIZ</p>
+                            <p class="textoDetalleInfoPie">Av. Huayna Capac y Pio Bravo. Matriz Cuenca</p>
+                        </div>
+                        <div class="detalleInfoPie">
+                            <p class="tituloDetalleInfoPie">SUCURSAL 1</p>
+                            <p class="textoDetalleInfoPie">Av. 10 de Agosto y Francisco Moscoso</p>
+                        </div>
+                        <div class="detalleInfoPie">
+                            <p class="tituloDetalleInfoPie">SUCURSAL 2</p>
+                            <p class="textoDetalleInfoPie">Av. de las Americas</p>
+                        </div>
+                    </div>
+                </div>        
+                    </p>
+                </article>
+                
+            </div>
+            <footer>
+                <div class="derechos">
+                    <p>Desarrollado por: Jose Calle, Pablo Calle, Marcelo Durazno, David Morales, Esteban Rosero</p>
+                    <p>Copyright &copy; 2019 Todos los derechos reservados</p>
+                </div>
+            </footer>
+    </body>
+</html>
