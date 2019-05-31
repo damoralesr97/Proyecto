42e37db8b2340b9016692b774bde78ab5617c867
David Morales <damoralesr97@outlook.com>
2019-05-31 03:26:48 -0500

    Cambio en conexionBD

--- config/conexionBD.php
+++ config/conexionBD.php
@@ -1,9 +1,9 @@
 <?php
 
     $db_servername = "localhost";
-    $db_username = "root";
-    $db_password = "";
-    $db_name = "prueba";
+    $db_username = "8617";
+    $db_password = "ferreteria";
+    $db_name = "8617";
 
     $conn = new mysqli($db_servername,$db_username,$db_password,$db_name);
     $conn->set_charset("utf8");
